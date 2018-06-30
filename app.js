efa658f6bb0ff03095567ef7a0688c41ff41608b
Joshua Spears <j.dspears@yahoo.com>
2018-06-30 16:52:23 -0700

    First commit.

---
+++ app.js
@@ -0,0 +1,17 @@
+// DEPENDENCIES
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  PORT = process.env.PORT || 3000,
+  app = express();
+
+// BODY PARSER CONFIG
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// SYNC THE SQL DB AND THEN LISTEN TO PORT
+// FORCE TRUE DROPS THE DATABASE IF ONE EXISTS
+
+app.listen(PORT, () => {
+  console.log(`Listening on PORT: ${PORT}`);
+}); // END THEN
